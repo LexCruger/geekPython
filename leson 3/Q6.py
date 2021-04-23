64eb296107fd892dec719815d10e2668be3f93e1
Lex Cruger <abramov.ao.it@gmail.com>
2021-04-23 19:21:08 +0500

    Q6

---
+++ leson 3/Q6.py
@@ -0,0 +1,11 @@
+def my_func(words):
+    result = ""
+    for word in words:
+        word = word[0].upper() + word[1:]
+        result += word + " "
+    return result
+
+
+string = input(
+    "Введите строку из слов, разделенных пробелом, где каждое слово состоит из латинских букв в нижнем регистре: ")
+print(my_func(string.split()))
